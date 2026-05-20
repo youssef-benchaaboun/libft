b6c53ef8e6b66f106a706085ddaa4c14309e2f2b
Youssef Ben Chaaboun <yoben-ch@c2r14s6.42madrid.com>
2026-05-20 16:34:46 +0200

    final_version

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: yoben-ch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/18 18:47:52 by yoben-ch          #+#    #+#             */
-/*   Updated: 2026/05/18 18:47:52 by yoben-ch         ###   ########.fr       */
+/*   Updated: 2026/05/20 16:33:24 by yoben-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ char	*ft_itoa(int n)
 	long	nb;
 
 	re = malloc(ft_len(n) + 1);
+	if (!re)
+		return (NULL);
 	nb = (long)n;
 	i = 0;
 	if (nb < 0)
